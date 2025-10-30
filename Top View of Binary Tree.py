34e9bcccc5c983304c8adade7b3faff2a91185da
Sai Kumar Adapa <96902883+saikumaradapa@users.noreply.github.com>
2025-10-30 22:30:53 +0530

    Create Top View of Binary Tree.py

---
+++ Top View of Binary Tree.py
@@ -0,0 +1,27 @@
+from collections import deque
+class Solution:
+    def topView(self, root):
+        if not root: return []
+        
+        q = deque([(root, 0)])
+        cols = {}
+        
+        while q:
+            curr, cn = q.popleft()
+            if cn not in cols:
+                cols[cn] = curr.data
+            
+            if curr.left:
+                q.append((curr.left, cn-1))
+            if curr.right:
+                q.append((curr.right, cn+1))
+        
+        res = []
+        for i in range(min(cols.keys()), max(cols.keys())+1):
+            res.append(cols[i])
+        return res
+
+'''
+  time complexity : O(n)
+  space complexity : O(n)
+'''

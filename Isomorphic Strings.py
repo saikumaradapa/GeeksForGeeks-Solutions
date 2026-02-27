d91c086117f7f6b6268d02090827c92e74bea2bd
Sai Kumar Adapa <96902883+saikumaradapa@users.noreply.github.com>
2026-02-27 20:16:54 +0530

    Update Isomorphic Strings.py

--- Isomorphic Strings.py
+++ Isomorphic Strings.py
@@ -11,5 +11,4 @@ class Solution:
             elif (s1[i] not in keys and s2[i] in values) or (s1[i] in keys and s2[i] not in values) or (keys[s1[i]] != s2[i] or values[s2[i]] != s1[i]):
                 return False
                 
-
         return True

fb2b8e1dcbb22847d3f63b71d979b2fceee84cbb
Sai Kumar Adapa <96902883+saikumaradapa@users.noreply.github.com>
2026-05-03 22:52:36 +0530

    Create Sort by Set Bit Count.py

---
+++ Sort by Set Bit Count.py
@@ -0,0 +1,40 @@
+class Solution:
+    def sortBySetBitCount(self, arr):
+        def get_bit_cnt(n):
+            cnt = 0
+            for i in range(33):
+                cnt += (n >> i) & 1
+            return cnt
+
+        bucket = defaultdict(list)
+        for n in arr:
+            bucket[get_bit_cnt(n)].append(n)
+
+        idx = 0
+        for i in range(32, -1, -1):
+            for n in bucket[i]:
+                arr[idx] = n
+                idx += 1
+
+'''
+    bucket sort by set bit count
+    iterate buckets from highest to lowest
+    stable since elements within each bucket maintain original order
+    time complexity : O(n)
+    space complexity : O(n)
+'''
+
+#################################################################################
+
+class Solution:
+    def sortBySetBitCount(self, arr):
+        arr.sort(key=lambda x: -bin(x).count('1'))
+
+'''
+    sort descending by set bit count
+    Python's sort is stable, so equal bit counts keep original order
+    bin(x).count('1') gives set bit count
+    time complexity : O(n log n)
+    space complexity : O(1)
+'''
+

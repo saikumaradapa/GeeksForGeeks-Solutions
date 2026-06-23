998b4000b993c3aa5e15e2209134a1d693f3aa4b
Sai Kumar Adapa <96902883+saikumaradapa@users.noreply.github.com>
2026-06-23 22:32:09 +0530

    Create Maximum Number of People Defeated.py

---
+++ Maximum Number of People Defeated.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxPeopleDefeated(self, p):
+        l, r = 1, p
+        ans = -1
+        while l <= r:
+            m = l + (r - l) // 2
+            if (m * (m + 1) * (2 * m + 1)) // 6 <= p:
+                ans = m
+                l = m + 1
+            else:
+                r = m - 1
+        return ans
+
+'''
+    sum of 1² + 2² + ... + n² = n(n+1)(2n+1)/6
+    binary search for largest n where sum <= p
+    time complexity : O(log p)
+    space complexity : O(1)
+'''

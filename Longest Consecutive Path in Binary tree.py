33091e365db9ace2735609a02988d4be6c020dd4
Sai Kumar Adapa <96902883+saikumaradapa@users.noreply.github.com>
2026-07-24 22:51:19 +0530

    Create Longest Consecutive Path in Binary tree.py

---
+++ Longest Consecutive Path in Binary tree.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestConsecutive(self, root):
+        self.ans = 1
+
+        def dfs(node, parent, length):
+            if not node:
+                return
+
+            if parent is not None and node.data == parent + 1:
+                length += 1
+            else:
+                length = 1
+
+            self.ans = max(self.ans, length)
+
+            dfs(node.left, node.data, length)
+            dfs(node.right, node.data, length)
+
+        dfs(root, None, 0)
+
+        return self.ans if self.ans > 1 else -1
